6cb2dc950310d6ddd3c81a9fa5e85bc480298d26
Ray He <devrayhe@gmail.com>
2021-09-25 16:14:38 +1000

    initial fetch data with gitignore hidden API key successful

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,37 @@
-var questionEl = document.getElementById("question");
-// document.querySelector("#question")
-var answer1El = document.getElementById("answer1");
-var answer2El = document.getElementById("answer2")
-var answer3El = document.getElementById("answer3")
-var answer4El = document.getElementById("answer4")
 
 
-// Stores questions in an array of objects
-var questions = [
-    {
-        question: "whats's our star?",
-        answer: ["the sun", "b", "c"],
-        correctAnswer: 0,
-    },
-    {
-        question: "whats's our star?",
-        answer: ["the sun", "b", "c"],
-        correctAnswer: 0,
-    },
-    {
-        question: "whats's our star?",
-        answer: ["the sun", "b", "c"],
-        correctAnswer: 0,
-    }
-];
+function handleSearchFormSubmit() {
+  //event.preventDefault();
 
-var currentQuestionIndex = 0;
+  // let searchInputVal = document.querySelector("#id").value.toLowerCase();
+  let city = searchInputVal = "sydney";
 
-function updateQuestion () {
-    var question = questions[currentQuestion];
+  if (!searchInputVal) {
+    console.error("You need a search input value!");
+    return;
+  }
 
-    questionEl.innerText = currentQuestion.question;
-    answer1El.innerText = currentQuestion.answers[0];
-    answer2El.innerText = currentQuestion.answers[1];
-    answer3El.innerText = currentQuestion.answers[2];
-    answer4El.innerText = currentQuestion.answers[3];
-
-    currentQuestionIndex++;
+  searchApi(city)
 }
 
-function checkAnswer (clickedAnswer) {
-    var currentQuestion = questions[currentQuestionIndex];
+// serachFormEl.addEventListener("submit", handleSearchFormSubmit);
+function searchApi(city) {
+  let APIKey = config.weatherAPIKey;
+  let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
-    if (currentQuestion.correctAnswer === clickedAnswer) {
-        console.log("correct answer");
-    }
-    else {
-        console.log("wrong answer");
-    }
-
-    currentQuestionIndex++;
-    updateQuestion ();
+  fetch(queryURL)
+    .then(function (response) {
+      if(!response.ok) {
+        throw response.json();
+      }
+      return response.json();
+    })
+    .then(function (searchRes) {
+      console.log(searchRes);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
 
-answer1El.addEventListener("click", function () {
-    checkAnswer(0);
-});
-answer2El.addEventListener("click", function () {
-    checkAnswer(1);
-});
-answer3El.addEventListener("click", function () {
-    checkAnswer(2);
-});
-answer4El.addEventListener("click", function () {
-    checkAnswer(3);
-});
-
-
-updateQuestion();
+handleSearchFormSubmit()
